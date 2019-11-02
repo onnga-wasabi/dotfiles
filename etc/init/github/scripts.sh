836cd311f53ec292a14665b2d5045622f2b036b5
onnga-wasabi <yuuto.onnga.23@gmail.com>
2019-11-02 14:11:45 +0900

    うわあああああああああああああああああああああああああああ

--- etc/init/github/scripts.sh
+++ etc/init/github/scripts.sh
@@ -107,7 +107,7 @@ install_dein() {
 install_tmux () {
   echo "Installing tmux..."
   VERSION="2.8"
-  if [ `which tmux` != "${HOME}/local/bin/tmux" ]
+  if [ "`which tmux`" != "${HOME}/local/bin/tmux" ]
   then
     install_libevent
     install_ncurses
