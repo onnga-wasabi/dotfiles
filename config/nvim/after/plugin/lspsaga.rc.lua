874de092b2a2194fda54c458ffa2333c836831da
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-01-26 17:45:00 +0000

    update

--- config/nvim/after/plugin/lspsaga.rc.lua
+++ config/nvim/after/plugin/lspsaga.rc.lua
@@ -1,42 +1,122 @@
 local ok, lspsaga = pcall(require, "lspsaga")
 if not ok then
-  return
+	return
 end
 
 local options = {
-  ui = {
-    -- currently only round theme
-    theme = "round",
-    -- this option only work in neovim 0.9
-    title = true,
-    -- border type can be single,double,rounded,solid,shadow.
-    border = "rounded",
-    winblend = 10,
-    expand = "",
-    collapse = "",
-    preview = " ",
-    code_action = "💡",
-    diagnostic = "🐞",
-    incoming = " ",
-    outgoing = " ",
-    colors = {
-      --float window normal background color
-      normal_bg = "#1d1536",
-      --title background color
-      title_bg = "#afd700",
-      red = "#e95678",
-      magenta = "#b33076",
-      orange = "#FF8700",
-      yellow = "#f7bb3b",
-      green = "#afd700",
-      cyan = "#36d0e0",
-      blue = "#61afef",
-      purple = "#CBA6F7",
-      white = "#d1d4cf",
-      black = "#1c1c19",
-    },
-    kind = {},
-  },
+	ui = {
+		theme = "round",
+		border = "rounded",
+		title = true,
+		winblend = 10,
+		expand = "",
+		collapse = "",
+		preview = " ",
+		code_action = "💡",
+		diagnostic = "🐞",
+		incoming = " ",
+		outgoing = " ",
+		colors = {
+			--float window normal bakcground color
+			normal_bg = "#1d1536",
+			--title background color
+			title_bg = "#afd700",
+		},
+		kind = {},
+	},
+	diagnostic = {
+		show_code_action = false,
+		show_source = false,
+		jump_num_shortcut = false,
+		keys = {
+			exec_action = "o",
+			quit = "q",
+			go_action = "g",
+		},
+	},
+	code_action = {
+		num_shortcut = true,
+		keys = {
+			quit = "q",
+			exec = "<CR>",
+		},
+	},
+	lightbulb = {
+		enable = false,
+		enable_in_insert = true,
+		-- cache_code_action = true,
+		sign = true,
+		sign_priority = 40,
+		virtual_text = true,
+	},
+	preview = {
+		lines_above = 0,
+		lines_below = 10,
+	},
+	scroll_preview = {
+		scroll_down = "<C-f>",
+		scroll_up = "<C-b>",
+	},
+	request_timeout = 2000,
+	finder = {
+		edit = { "o", "<CR>" },
+		vsplit = "s",
+		split = "i",
+		tabe = "t",
+		quit = { "q", "<ESC>" },
+	},
+	definition = {
+		edit = "<C-c>o",
+		vsplit = "<C-c>v",
+		split = "<C-c>i",
+		tabe = "<C-c>t",
+		quit = "q",
+		close = "<Esc>",
+	},
+	rename = {
+		quit = "<C-c>",
+		exec = "<CR>",
+		mark = "x",
+		confirm = "<CR>",
+		in_select = true,
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = " ",
+		hide_keyword = true,
+		show_file = true,
+		folder_level = 2,
+		respect_root = false,
+		color_mode = true,
+	},
+	outline = {
+		win_position = "right",
+		win_with = "",
+		win_width = 30,
+		show_detail = false,
+		auto_preview = false,
+		auto_refresh = false,
+		auto_close = true,
+		custom_sort = nil,
+		keys = {
+			jump = "o",
+			expand_collapse = "u",
+			quit = "<ESC>",
+		},
+	},
+	callhierarchy = {
+		show_detail = false,
+		keys = {
+			edit = "e",
+			vsplit = "s",
+			split = "i",
+			tabe = "t",
+			jump = "o",
+			quit = "<ESC>",
+			expand_collapse = "u",
+		},
+	},
+	server_filetype_map = {},
 }
 lspsaga.setup(options)
 
