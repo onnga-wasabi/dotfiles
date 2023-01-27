a8f20b4d9d988543856b9eae22a3ceb9d0ee43e2
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-01-27 12:55:05 +0000

    update

--- config/nvim/lua/base.lua
+++ config/nvim/lua/base.lua
@@ -7,10 +7,8 @@ vim.g.mapleader = " " -- Set <Space> as <Leader>
 -- basic shortcuts
 set_keymap("n", "<c-w>n", "<nop>", opts)
 set_keymap("n", "s", "<nop>", opts)
-
 set_keymap("n", "j", "gj", opts)
 set_keymap("n", "k", "gk", opts)
-set_keymap("n", "<c-s>", "<c-i>", opts)
 set_keymap("n", "<c-c>", "<cmd>noh<cr>:w<cr>", opts)
 set_keymap("n", "<c-p>", '<cmd>echo expand("%")<cr>', opts)
 set_keymap("n", "<leader>r", "<cmd>source ~/.config/nvim/init.lua<cr>", opts)
@@ -18,6 +16,13 @@ set_keymap("n", "<leader>r", "<cmd>source ~/.config/nvim/init.lua<cr>", opts)
 -- windows and buffers
 set_keymap("n", "<c-w>v", "<c-w>v<c-w>l", opts)
 set_keymap("n", "<c-w>s", "<c-w>s<c-w>j", opts)
+
+set_keymap("n", "<c-w>l", "<cmd>:w<cr><c-w>l", opts)
+set_keymap("n", "<c-w>j", "<cmd>:w<cr><c-w>j", opts)
+set_keymap("n", "<c-w>k", "<cmd>:w<cr><c-w>k", opts)
+set_keymap("n", "<c-w>h", "<cmd>:w<cr><c-w>h", opts)
+set_keymap("n", "<c-o>", "<cmd>:w<cr><c-o>", opts)
+set_keymap("n", "<c-s>", "<cmd>:w<cr><c-i>", opts)
 
 set_keymap("n", ",", "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>", opts)
 set_keymap("n", "sp", "<cmd>bn<cr>", opts)
@@ -40,8 +45,9 @@ vim.api.nvim_create_user_command("Memo", function(_)
 end, {})
 -- }}} end of Util Functions
 
--- File Types
-vim.cmd([[ au BufNewFile,BufRead *.rb,*.rbw,Brewfile setf ruby ]])
-vim.cmd([[ autocmd BufWritePost * if expand('%') != '' && &buftype !~ 'nofile' | mkview | endif ]])
-vim.cmd([[ autocmd BufRead * if expand('%') != '' && &buftype !~ 'nofile' | silent loadview | endif ]])
-vim.cmd([[ set viewoptions-=options ]])
+-- cusor 記憶
+vim.cmd([[
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+endif
+]])
