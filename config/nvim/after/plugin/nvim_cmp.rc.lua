a9fc59ab559c8b190eafec5ee982ff4308ad8c6f
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-03-10 22:42:22 +0900

    update

--- config/nvim/after/plugin/nvim_cmp.rc.lua
+++ config/nvim/after/plugin/nvim_cmp.rc.lua
@@ -39,12 +39,12 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	formatting = { -- refer to: https://github.com/onsails/lspkind.nvim
+	formatting = {
+		-- refer to: https://github.com/onsails/lspkind.nvim
 		format = lspkind.cmp_format({
 			mode = "symbol", -- show only symbol annotations
 			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
 			-- The function below will be called before any actual modifications from lspkind
 			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
 			-- before = function (entry, vim_item)
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,37 +1,31 @@
 local options = {
-  encoding = "utf-8",
-  fileencoding = "utf-8",
-  title = true,
-
-  ignorecase = true,
-  smartcase = true,
-
-  backup = false,
-  writebackup = false,
-  swapfile = false,
-
-  autoindent = true,
-  smartindent = true,
-  cindent = true,
-  expandtab = true,
-
-  termguicolors = true,
-  cursorline = true,
-  number = true,
-  numberwidth = 4,
-  signcolumn = "yes",
-  wrap = false,
-  showtabline = 2,
-  shiftwidth = 2,
-  tabstop = 2,
-
-  timeoutlen = 300,
-  updatetime = 300,
-
-  laststatus = 3,
-  regexpengine = 1,
+	encoding = "utf-8",
+	fileencoding = "utf-8",
+	title = true,
+	ignorecase = true,
+	smartcase = true,
+	backup = false,
+	writebackup = false,
+	swapfile = false,
+	autoindent = true,
+	smartindent = true,
+	cindent = true,
+	expandtab = true,
+	termguicolors = true,
+	cursorline = true,
+	number = true,
+	numberwidth = 4,
+	signcolumn = "yes",
+	wrap = false,
+	showtabline = 2,
+	shiftwidth = 2,
+	tabstop = 2,
+	timeoutlen = 300,
+	updatetime = 300,
+	laststatus = 3,
+	regexpengine = 2, -- 正規表現のエンジン
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
