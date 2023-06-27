3c763f6c388cf6146c83c9cfb3e5d6b246f94638
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-06-27 15:44:16 +0000

    update

--- bin/brew.sh
+++ bin/brew.sh
@@ -4,6 +4,10 @@ CURRENT=$(cd $(dirname $0) && pwd)
 source ${CURRENT}/share.sh
 
 install_brew_bundle () {
+  echo "Cleaning Up Brew Bundles..."
+  brew cleanup
+  cd ${DOTPATH}/etc/brew && brew bundle cleanup --force
+
   echo "Installing Brew Bundles..."
   cd ${DOTPATH}/etc/brew && brew bundle
   echo
