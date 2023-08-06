a3869300b25db8f7636e2fbd4f19f11160051517
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-08-07 00:32:43 +0900

    update

--- config/nvim/after/plugin/notic.rc.lua
+++ config/nvim/after/plugin/notic.rc.lua
@@ -54,9 +54,9 @@ local config = {
 		-- NOTE: If you enable messages, then the cmdline is enabled automatically.
 		-- This is a current Neovim limitation.
 		enabled = true, -- enables the Noice messages UI
-		view = "notify", -- default view for messages
-		view_error = "mini", -- view for errors
-		view_warn = "notify", -- view for warnings
+		view = "mini", -- default view for messages
+		view_error = "notify", -- view for errors
+		view_warn = "mini", -- view for warnings
 		view_history = "messages", -- view for :messages
 		view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
 	},
