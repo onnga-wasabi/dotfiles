7113e23048cc8db863f9e63c8ebfa70157af19c3
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-08-26 16:42:32 +0900

    update

--- config/nvim/after/plugin/nvim_cmp.rc.lua
+++ config/nvim/after/plugin/nvim_cmp.rc.lua
@@ -55,12 +55,12 @@ cmp.setup({
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline({ "/", "?" }, {
--- 	mapping = cmp.mapping.preset.cmdline(),
--- 	sources = {
--- 		{ name = "buffer" },
--- 	},
--- })
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
--- zshrc
+++ zshrc
@@ -123,6 +123,17 @@ zd() {
   dir=$(find ${1:-.} -path '*/\.*' -prune -o -type d -print 2> /dev/null | fzf +m) &&
   cd "$dir"
 }
+zgd() {
+  local dir
+  local workdir
+  if [[ $1 ]] then;
+      workdir=$1
+  else
+      workdir=${HOME}/"workspace"
+  fi
+  dir=$(rg --files --hidden --follow --glob "**/.git/*" $workdir | rev | cut -d '/' -f 3- | rev | sort | uniq | fzf +m) &&
+  cd "$dir"
+}
 
 # open in tmux pane
 export FZF_TMUX=1
