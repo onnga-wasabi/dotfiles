9dbf2d9c2dfd7ea9099ca959919d24330106dbf8
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-09-23 14:16:04 +0900

    update

---
+++ config/nvim/lua/plugins/nvim_lint.lua
@@ -0,0 +1,20 @@
+return {
+	-- https://github.com/mfussenegger/nvim-lint
+	"mfussenegger/nvim-lint",
+	config = function()
+		-- setup linters
+		require("lint").linters_by_ft = {
+			go = {
+				"golangcilint",
+			},
+		}
+
+		-- check lint autocmd
+		vim.api.nvim_create_autocmd({
+			"BufReadPost",
+			"BufWritePre",
+		}, {
+			callback = function() require("lint").try_lint() end,
+		})
+	end,
+}
--- config/nvim/lua/plugins/smoothcursor.lua
+++ config/nvim/lua/plugins/smoothcursor.lua
@@ -8,16 +8,16 @@ return {
 		linehl = nil, -- highlight sub-cursor line like 'cursorline', "CursorLine" recommended
 		type = "default", -- define cursor movement calculate function, "default" or "exp" (exponential).
 		fancy = {
-			enable = false, -- enable fancy mode
+			enable = true, -- enable fancy mode
 			head = { cursor = "▷", texthl = "SmoothCursor", linehl = nil },
 			body = {
 				-- { cursor = "", texthl = "SmoothCursorRed" },
 				-- { cursor = "", texthl = "SmoothCursorOrange" },
-				{ cursor = "●", texthl = "SmoothCursorYellow" },
-				{ cursor = "●", texthl = "SmoothCursorGreen" },
+				-- { cursor = "●", texthl = "SmoothCursorYellow" },
+				{ cursor = "•", texthl = "SmoothCursorGreen" },
 				{ cursor = "•", texthl = "SmoothCursorAqua" },
-				{ cursor = ".", texthl = "SmoothCursorBlue" },
-				{ cursor = ".", texthl = "SmoothCursorPurple" },
+				{ cursor = "•", texthl = "SmoothCursorBlue" },
+				{ cursor = "•", texthl = "SmoothCursorPurple" },
 			},
 			tail = { cursor = nil, texthl = "SmoothCursor" },
 		},
