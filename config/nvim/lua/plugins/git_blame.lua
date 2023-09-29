45de216ebd33f38e6e5bd4ba9e98038ca1614212
onnga-wasabi <yuuto.onnga.23@gmail.com>
2023-09-29 11:19:28 +0900

    update

--- config/nvim/lua/plugins/git_blame.lua
+++ config/nvim/lua/plugins/git_blame.lua
@@ -4,4 +4,10 @@ return {
 	opts = {
 		enable = true,
 	},
+	config = function(_, opts)
+		vim.g.gitblame_virtual_text_column = 130
+
+		local git_blame = require("gitblame")
+		git_blame.setup(opts)
+	end,
 }
